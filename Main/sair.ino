37e1fe7af9a6becbf80b613d8837e829c10cd551
Anderson Daniel <84481208+antiderson@users.noreply.github.com>
2021-12-09 21:26:32 -0300

    Update sair.ino

--- Main/sair.ino
+++ Main/sair.ino
@@ -1,23 +1,23 @@
 // Função Sair, onde finaliza o Game //
 void sair ()
 {
-  char text_sair[6] = {'X', 'a', 'u', 'u', 'u', '!'};
+  char text_sair[9] = {'A', 't', 'e', ' ', 'L', 'o', 'g', 'o', '!'};
   
   lcd.fillScreen(ST77XX_BLACK);
-  lcd.setTextSize(4);
+  lcd.setTextSize(3);
   lcd.setTextColor(WHITE);
   lcd.setCursor(50, 110);
   for(int i = 0; i < 6; i++)
   { 
     lcd.print(text_sair[i]);
-    delay(500);
+    delay(250);
   }
   
   for(int z = 1, y = 0; z < 14; z++)
   { 
-    lcd.fillRect(0,y,240,20,CYAN);
+    lcd.fillRect(0,y,240,20,RED);
     delay(200);
-    lcd.fillRect(0,y,240,20,BLUE);
+    lcd.fillRect(0,y,240,20,RED);
     delay(200);
     y += 20;
   }
